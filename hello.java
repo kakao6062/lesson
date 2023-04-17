742963c203a0e57356acf029624391ecba5a1516
toshiko <toshihiko.sd104@gmail.com>
2023-04-17 21:40:32 +0900

    toshiko change hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         //以下にSee you, world!と出力するコードを追加
+        System.out.println("See you, world!");
     }
 }
